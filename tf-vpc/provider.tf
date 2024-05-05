c62bb2182912c2def32004734145a56cf2b8b9fe
bensooraj <bensooraj@users.noreply.github.com>
2024-05-06 01:31:41 +0530

    base file structure

---
+++ tf-vpc/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-remote-backend-s3"
+    key    = "aws-vpc-terraform-ga/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
